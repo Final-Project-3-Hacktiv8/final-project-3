234edafcc70c7f33405bdbb060f05c948582f23b
Krisna Lavendra <krisnalavendrairawan@gmail.com>
2023-05-19 21:58:10 +0700

    add getalltransaction for admin

--- routers/transactionRouters.js
+++ routers/transactionRouters.js
@@ -4,11 +4,11 @@ const {authentication} = require('../middlewares/authentication')
 const { adminAuthorization, transactionAuthorization } = require('../middlewares/authorization')
 
 router.use(authentication)
+router.use('/all',adminAuthorization)
+router.get('/all', transactionHistoryController.getAllTransaction)
 router.get('/', transactionHistoryController.getTransactionByUser)
 router.post('/', transactionHistoryController.createTransaction)
 router.use('/:id', transactionAuthorization)
 router.get('/:id', transactionHistoryController.getTransactionById)
-router.use(adminAuthorization)
-router.get('/all', transactionHistoryController.getAllTransaction)
 
 module.exports = router
