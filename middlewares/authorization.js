f73994ed2051a60a5e0e00860672e060fbf7b533
Muhammad Nur Hamada <47131367+XvcreAm@users.noreply.github.com>
2023-05-20 17:11:24 +0700

    add productAuthorization

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -54,10 +54,37 @@ async function adminAuthorization(req, res, next) {
       console.log(err);
       return res.status(401).json(err);
     }
+    
+    //product authorization
+function productAuthorization (req, res, next) {
+    const id = req.params.id
+    Product.findOne({
+        where : {
+            id : id
+        }
+    })
+        .then(product => {
+            if (!product) {
+                return res.status(404).json({
+                    message: 'Product not found',
+                    devMessge: `Product with id ${id} not found`
+                })
+            }
+            return next()
+        })
+        .catch(err => {
+            return res.status(500).json({
+                message: 'Internal server error',
+                devMessage: err.message
+            })
+        })
+
+}
   }
 
 
 module.exports = {
     userAuthorization,
-    adminAuthorization
+    adminAuthorization,
+    productAuthorization
 }
