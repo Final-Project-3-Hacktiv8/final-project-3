0bd19988f19967b6734d2696e3297647107ab2e6
Krisna Lavendra <krisnalavendrairawan@gmail.com>
2023-05-20 15:08:18 +0700

    add admin authorization

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -1,5 +1,6 @@
 
 const {User, Product, TransactionHistory} = require('../models')
+const {verifyToken} = require('../helpers/jwt')
 
 function userAuthorization (req, res, next) {
     const id = res.locals.user.id
@@ -20,6 +21,43 @@ function userAuthorization (req, res, next) {
         })
 }
 
+//admin authorization
+async function adminAuthorization(req, res, next) {
+    try {
+      // const token = req.headers.access_token
+      const token = req.get("token");
+      const Userdecoded = verifyToken(token);
+      User.findOne({
+        where: {
+          id: Userdecoded.id,
+          email: Userdecoded.email,
+          role: "admin",
+        },
+      })
+        .then((user) => {
+          if (!user) {
+            return res.status(401).json({
+              message: "User not authenticated",
+              devMessage: `User with id ${Userdecoded.id} not admin`,
+            });
+          }
+          res.locals.user = user;
+          return next();
+        })
+        .catch((err) => {
+          return res.status(500).json({
+            message: "Internal server error",
+            devMessage: err.message,
+          });
+        });
+    } catch (err) {
+      console.log(err);
+      return res.status(401).json(err);
+    }
+  }
+
+
 module.exports = {
     userAuthorization,
+    adminAuthorization
 }
